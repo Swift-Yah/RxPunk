c2ad71e5e3df33a63c57716c477c21acd00d1f1b
Rafael da Silva Ferreira <rafael@swift-yah.io>
2017-05-23 23:34:23 -0300

    Removes boilerplates at ViewController and AppDelegate created by Xcode

---
+++ RxPunk/HomeController.swift
@@ -0,0 +1,11 @@
+//
+//  HomeController.swift
+//  RxPunk
+//
+//  Created by Rafael Ferreira on 5/23/17.
+//  Copyright © 2017 Swift Yah. All rights reserved.
+//
+
+import class UIKit.UIViewController
+
+final class HomeController: UIViewController { }
