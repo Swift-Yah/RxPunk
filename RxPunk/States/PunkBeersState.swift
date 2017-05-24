2c81731c7569610f8eda65be03ee0196215dfd0c
Rafael da Silva Ferreira <rafael@swift-yah.io>
2017-05-24 05:05:59 -0300

    Add static elements to PunkBeersState
    These static members will be in charge of control the state itself.

--- RxPunk/States/PunkBeersState.swift
+++ RxPunk/States/PunkBeersState.swift
@@ -8,8 +8,39 @@
 
 struct PunkBeersState: Mutable {
     var shouldLoadNextPage: Bool = true
+    var hasNextPage: Bool = false
     var beers: Version<[Beer]> = Version([])
     var failure: PunkAPIError? = nil
+}
+
+// MARK: Static elements
+
+extension PunkBeersState {
+    static let initial = PunkBeersState()
+
+    static func reduce(state: PunkBeersState, command: PunkCommand<DefaultPunkAPI>) -> PunkBeersState {
+        switch command {
+        case .punkReceivedResponseReceived(let result):
+            switch result {
+            case let .success((beers, hasNext)):
+                return state.mutate(transform: {
+                    $0.shouldLoadNextPage = false
+                    $0.beers = Version($0.beers.value + beers)
+                    $0.hasNextPage = hasNext
+                    $0.failure = nil
+                })
+            case let .failure(error):
+                return state.mutateOne(transform: { $0.failure = error })
+            }
+
+        case .loadMoreItems:
+            return state.mutate(transform: {
+                if $0.failure == nil {
+                    $0.shouldLoadNextPage = true
+                }
+            })
+        }
+    }
 }
 
 // MARK: Computed variables
