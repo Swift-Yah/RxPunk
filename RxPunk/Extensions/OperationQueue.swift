34d79ec68fa7cfb75a2da4938309e0f42a23d7b6
Rafael da Silva Ferreira <rafael@swift-yah.io>
2017-05-24 22:22:08 -0300

    Add convenience inits to OperationQueueScheduler and OperationQueue

---
+++ RxPunk/Extensions/OperationQueue.swift
@@ -0,0 +1,16 @@
+//
+//  OperationQueue.swift
+//  RxPunk
+//
+//  Created by Rafael Ferreira on 5/24/17.
+//  Copyright © 2017 Swift Yah. All rights reserved.
+//
+
+import class Foundation.OperationQueue
+
+extension OperationQueue {
+    convenience init(maxConcurrentOperationCount: Int) {
+        self.init()
+        self.maxConcurrentOperationCount = maxConcurrentOperationCount
+    }
+}
